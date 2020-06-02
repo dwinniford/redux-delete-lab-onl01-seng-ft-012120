7aa5b1b84c05108dac8f05e44cb2f54ce6aa8223
dwinniford <dwinniford@gmail.com>
2020-06-02 08:52:31 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -5,7 +5,10 @@ class Band extends Component {
   render() {
     return(
       <div>
-        Band Component
+        <li>
+          {this.props.band.name}
+          <button id={this.props.band.id} onClick={event => this.props.deleteBand(event.target.id)}>Remove</button>
+        </li>
       </div>
     );
   }
